a9ed68517bf3d18c87f921b2c6f17d794d769eea
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-17 11:44:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,15 +6,25 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+<<<<<<< HEAD
     if BRANDS.include?(brand)
     else 
       BRANDS << brand
     end
+=======
+>>>>>>> 363bb03735d549a03212de5da2af81a325e7a8f4
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+<<<<<<< HEAD
 
+=======
+def brand=(brand)
+  @brand = brand
+  BRANDS << brand
+end
+>>>>>>> 363bb03735d549a03212de5da2af81a325e7a8f4
 end
